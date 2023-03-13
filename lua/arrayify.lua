9d04f9e8aa7dd990c07a3c7e5138cd3f2b1aea9b
Seth Buchanan <seth.buchanan50@gmail.com>
2023-03-13 16:47:20 -0500

    typecasting in progress

--- lua/arrayify.lua
+++ lua/arrayify.lua
@@ -2,13 +2,17 @@ vim.api.nvim_create_user_command("Arrayify", function ()
 
     local syntax = SetSyntax()
 
+    local input = Input()
     if syntax["pattern"] == "standard" then
-        local input = Input()
         local output = StandardFormat(input, syntax)
-        WriteAtCursor(output)
+        WriteAfterCursor(output)
+    else if syntax["pattern"] == "java" then
+        local output = JavaIsAnnoying(input, syntax)
+        WriteAfterCursor(output)
     else
         print "pattern not defined"
     end
+end
 
 end, {})
 
@@ -31,8 +35,7 @@ function StandardFormat(inputArray, syntax)
     local output = syntax["brackets"][1]
 
     for _, input in ipairs(inputArray) do
-
-        if IsNotInteger(input) and not InList(syntax["keywords"], input) then
+        if not IsInteger(input) and not InList(syntax["keywords"], input) then
             input = string.format("%s%s%s",
             syntax["quote"], input, syntax["quote"])
         end
@@ -40,6 +43,43 @@ function StandardFormat(inputArray, syntax)
     end
     output =  output:sub(1, #output - string.len(syntax["deliniator"]))
     return output .. syntax["brackets"][2]
+end
+
+function JavaIsAnnoying(inputArray, syntax)
+    local output = syntax["brackets"][1]
+    local type = nil
+    local highestBits = 0
+
+    if IsInteger(inputArray[1]) then
+        type = "int"
+    elseif InList(syntax["keywords"], inputArray[1]) then
+        type = syntax["types"][2]
+    else
+        type = syntax["types"][3]
+    end
+
+    for _, input in ipairs(inputArray) do
+        if type == "int" then
+            local bits = GiveBits(input)
+            if highestBits < bits then
+                highestBits = bits
+            end
+        end
+        if not IsInteger(input) and not InList(syntax["keywords"], input) then
+            input = string.format("%s%s%s",
+            syntax["quote"], input, syntax["quote"])
+        end
+        output = string.format("%s%s%s", output, input, syntax["deliniator"])
+    end
+
+    output = output:sub(1, #output - string.len(syntax["deliniator"]))
+    return string.format("%s[] array = new %s[]%s%s",
+    type, type, output, syntax["brackets"][2])
+end
+
+-- returns the number of bits in a number
+function GiveBits(number)
+    local bits = math.floor(math.log(2,number)) + 1
 end
 
 function SetSyntax()
@@ -63,6 +103,12 @@ function SetSyntax()
     elseif filetype == "java" then
         syntax["brackets"]  = { "{", "};" }
         syntax["keywords"]  = {"true", "false"}
+        syntax["types"]     = {{}, "boolean", "String"}
+        syntax["types"][1]["8"] = "Byte"
+        syntax["types"][1]["16"] = "Short"
+        syntax["types"][1]["32"] = "Int"
+        syntax["types"][1]["64"] = "Long"
+        syntax["pattern"]   = "typeCasted"
 
     -- Python
     elseif filetype == "py" then
@@ -90,8 +136,8 @@ function SetSyntax()
 end
 
 -- returns true if string contains anything other than an integer
-function IsNotInteger(str)
-    return (str == "" or str:find("%D"))
+function IsInteger(str)
+    return not (str == "" or str:find("%D"))
 end
 
 -- Iterator function that sees if "str" is in "array"
@@ -105,7 +151,7 @@ function InList(array, str)
 end
 
 -- Finds where cursor is currently and prints output there
-function WriteAtCursor(str)
+function WriteAfterCursor(str)
   local pos = vim.api.nvim_win_get_cursor(0)[2]
   local line = vim.api.nvim_get_current_line()
   local newline = line:sub(0, pos + 1 ) .. str .. line:sub(pos + 2)
